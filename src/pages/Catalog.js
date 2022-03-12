8df544d16d133542316bf5e635f257f2d840d871
Ricardo Atakiama <atakiama@usp.br>
2022-03-12 03:47:31 -0300

    Desenvolvendo o grid system e elçementos dos cards

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,14 +1,25 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Paper, Typography } from "@mui/material";
+import { borderRadius } from "@mui/system";
 
 import MainLayout from "../layouts/Main";
 
 import { useCart } from "../models/Cart";
+import { useCatalog } from "../models/Catalog";
 
 function CatalogPage() {
-  const { cart } = useCart();
+  const { catalog } = useCatalog();
+  console.log(catalog)
+ 
+  const { cart, addToCart } = useCart();
+
+  console.log(cart)
 
   const cartNItems = cart.items.length;
   const cartTotal = `R$ ${cart.total.toFixed(2)}`;
+
+  // const handleClick = () => {
+  //   addToCart()
+  // }
 
   return (
     <MainLayout
@@ -16,8 +27,25 @@ function CatalogPage() {
       footerLabel={`${cartTotal} (${cartNItems} itens)`}
     >
       <Box sx={{ p: 2, flex: 1 }}>
-        <Typography variant="body1">TODO: grid de produtos</Typography>
+        {/* <Typography variant="h6" fontWeight="bold">{{}}</Typography> */}
+        
+        <Grid container>
+          {catalog.products.map((elem, index) => (
+            <Grid item md={3} px={2} py={1}>
+              <Box sx={{ p: 2, mt: 2, 
+                height: "180px", 
+                // background: `url(${elem.imageUrl})`,
+                backgroundColor: "#CECFD1",
+                borderRadius: "10px 10px 0 0"}} >
+              </Box>
+              <Paper sx={{ p:1, borderRadius: "0 0 10px 10px", height: "60px" }}>
+                <Typography variant="body2">{elem.name}</Typography>
+              </Paper>
+            </Grid> 
+          ))}
+        </Grid>
       </Box>
+      
     </MainLayout>
   );
 }
