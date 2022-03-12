59b0dee9a7ea2e525e56cd6cb61bdaaf3736a673
Ricardo Atakiama <atakiama@usp.br>
2022-03-12 19:12:52 -0300

    Função de adicionar ao carrinho considera a quantidade

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -3,30 +3,22 @@ import React, { createContext, useContext, useState } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [items, setItems] = useState([
-    {
-      "id": "6400c151-2bd6-4446-b0a5-4f1e33954051",
-      "name": "Espresso pequeno",
-      "imageUrl": "./images/6400c151-2bd6-4446-b0a5-4f1e33954051.jpg",
-      "price": "5",
-      "categoryName": "Bebidas quentes"
-    },
-    {
-      "id": "6a84952b-1111-43be-bc3c-0823466c0363",
-      "name": "Mochaccino",
-      "imageUrl": "./images/6a84952b-1111-43be-bc3c-0823466c0363.jpg",
-      "price": "8",
-      "categoryName": "Bebidas quentes"
-    },
-  ]);
+  const [items, setItems] = useState([]);
 
   const getItemTotal = () => 
-    items.reduce((acc, item) => acc + parseFloat(item.price), 0);
-    console.log("TODO: should compute card total");
-
+    items.reduce((acc, item) => acc + parseFloat(item.price) * item.quantity, 0);
+  
   const addToCart = (product) => {
-    console.log("TODO: add to cart product", product);
-    setItems([ ...items, product ]);
+    const foundItem = items.find(e => e.name === product.name);
+
+    if (foundItem) {
+      foundItem.quantity++;
+      setItems([...items]);
+    } else {
+      product.quantity = 1;
+      setItems([...items, product]);
+    }
+    console.log(items)
   }
 
   return (
