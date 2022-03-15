6ad28fb85401ed3dfc1264b515c5d385717ef5c8
Ricardo Atakiama <atakiama@usp.br>
2022-03-15 17:25:51 -0300

    Funções globais criadas em utils.Functions.js: sortByField, textToVariableName e VariableNameToText. Outras funções de formatação de moeda foram passadas para lá

---
+++ src/utils/Functions.js
@@ -0,0 +1,24 @@
+export function sortByField(array, field) {
+  array.sort((a, b) => {
+    const x = a[field].toString().toLowerCase() || '';
+    const y = b[field].toString().toLowerCase() || '';
+    
+    return x > y ? 1 : x < y ? -1 : 0;
+  });
+}
+
+export function textToVariableName(text) {
+  return text.toString().toLowerCase().replace(' ', '-')
+}
+
+export function variableNameToText(variableName) {
+  return (variableName.charAt(0).toUpperCase() + variableName.substr(1).toLowerCase()).replace('-', ' ')
+}
+
+export function removeZerosCurrency(value) {
+  return value.replace(/,00|.00/,  '')
+}
+
+export function formatCurrency(value) {
+  return `R$ ${Number(value).toFixed(2).replace('.', ',')}`
+}
