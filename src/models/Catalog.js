9f029a1457cbf74ba1622bedc4cedce12fe9d7ee
Ricardo Atakiama <atakiama@usp.br>
2022-03-15 17:38:58 -0300

    O Context do Catalog agora categoriza os produtos antes de enviar para a página de catálogo

--- src/models/Catalog.js
+++ src/models/Catalog.js
@@ -2,11 +2,25 @@ import React, { createContext, useContext } from "react";
 
 import ExampleCatalog from "../assets/ExampleCatalog.json";
 
+import { textToVariableName } from "../utils/Functions"
+
 export const CatalogContext = createContext();
 
 export const CatalogProvider = ({ children }) => {
+  const categorize = (products) => {
+    let productsInCategories = {};
+    
+    products.forEach((product) => {
+      const newField = textToVariableName(product.categoryName);
+      productsInCategories[newField] = productsInCategories[newField] || []
+      productsInCategories[newField].push(product);
+    })
+
+    return productsInCategories;
+  }
+
   const catalog = {
-    products: ExampleCatalog.products,
+    products: { categories: categorize(ExampleCatalog.products) },
   };
 
   return (
--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,24 +1,16 @@
 import { Box, Grid, Typography } from "@mui/material";
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import GridCardItem from "../components/GridCardItem";
 
 import MainLayout from "../layouts/Main";
 
 import { useCart } from "../models/Cart";
 import { useCatalog } from "../models/Catalog";
+import { formatCurrency, variableNameToText } from "../utils/Functions";
 
 function CatalogPage() {
   const { catalog } = useCatalog();
-
-  useEffect(() => {
-    catalog.products.sort((a, b) => {
-      if (a.categoryName > b.categoryName) return 1;
-      if (a.categoryName < b.categoryName) return -1;
-      return 0;
-    });
-  }, [catalog]);
- 
-  const { cart, addToCart, getItemsQuantity, formatCurrency } = useCart();
+  const { cart, addToCart, getItemsQuantity } = useCart();
 
   const cartNItems = getItemsQuantity();
   const cartTotal = formatCurrency(cart.total);
@@ -32,15 +24,16 @@ function CatalogPage() {
     >
       <Box sx={{ p: 2, pb: 4, flex: 1 }}>
         <Grid container>
-          {catalog.products.map((elem, index) => (
-            <Fragment key={elem.id}>
-              {elem.categoryName !== catalog.products[index-1]?.categoryName && 
+          {Object.keys(catalog.products.categories).map((category) => ( //cada category é só uma chave
+            <Fragment key={category}>
               <Typography variant="h6" component="div" sx={{px: 2, pt: 6, pb: 2, width: "100%", fontWeight: "bold"}}>
-                {elem.categoryName}
-              </Typography>}
-              <Grid item md={3} px={2} py={1} key={elem.id} onClick={(() => handleClick(elem))}>
-                <GridCardItem item={elem} />
-              </Grid>
+                {variableNameToText(category)}
+              </Typography>
+              {catalog.products.categories[category].map((item) => (
+                <Grid item md={3} px={2} py={1} key={item.id} onClick={(() => handleClick(item))}>
+                  <GridCardItem item={item} />
+                </Grid>
+              ))}
             </Fragment>
           ))}
         </Grid>
