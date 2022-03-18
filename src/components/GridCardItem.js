37e6c1582df04b5133cf87fa3bf0073cc893869a
Ricardo Atakiama <atakiama@usp.br>
2022-03-18 06:01:35 -0300

    Cursor pointer nos grid items

--- src/components/GridCardItem.js
+++ src/components/GridCardItem.js
@@ -16,13 +16,14 @@ const GridCardItem = ({ item }) => {
           display: "flex",
           flexDirection: "column",
           justifyContent: "space-between",
-          p: 1, mt: 2,
+          p: 1, m: 2, mb: 0,
           height: "180px",
           backgroundColor: "#CECFD1",
           borderRadius: "10px 10px 0 0",
           backgroundImage: "url(" + require(`../assets/${item?.imageUrl.split('./')[1]}`).default + ")",
           backgroundPosition: 'center center no-repeat',
           backgroundSize: 'cover',
+          cursor: "pointer"
         }} 
       >
         <Box 
@@ -52,7 +53,7 @@ const GridCardItem = ({ item }) => {
         </Box>
       </Box>
       <Paper component="div"
-        sx={{ p:1, borderRadius: "0 0 10px 10px", height: "60px", boxShadow: "2px 2px 16px 2px rgba(0, 0, 0, 0.1)" }}>
+        sx={{ p:1, m: 2, mt: 0, borderRadius: "0 0 10px 10px", height: "50px", boxShadow: "2px 2px 16px 2px rgba(0, 0, 0, 0.1)", cursor: "pointer" }} >
         <Typography fontSize={12} fontWeight="medium" >
           {item.name}
         </Typography>
