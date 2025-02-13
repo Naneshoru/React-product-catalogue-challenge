537082d72043f8ba86603217b8daf431226a5ab9
Ricardo Atakiama <ricardo_cssg@hotmail.com>
2025-02-13 18:21:17 -0300

    fix: função de adicionar ao carrinho

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -3,19 +3,32 @@ import React, { createContext, useContext, useState } from "react";
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(null);
 
   const getItemsQuantity = () => 
-    items.reduce((acc, item) => acc + item.quantity, 0);
+    items?.reduce((acc, item) => acc + item.quantity, 0) ?? 0;
 
   const getItemTotal = () => 
-    items.reduce((acc, item) => acc + parseFloat(item.price) * item.quantity, 0);
+    items?.reduce((acc, item) => acc + parseFloat(item.price) * item.quantity, 0) ?? 0; 
 
   const addToCart = (product) => {
-    const filteredItems = items.filter(item => item.name !== product.name);
-  
-    product.quantity = product.quantity + 1 || 1;
-    setItems([...filteredItems, product]);
+    setItems(prev => {
+      if (prev) {
+        const productFound = prev.find((item) => item.name === product.name)
+        if (productFound) {
+          return prev.map((item) => {
+            if (item.name === product.name) {
+              return { ...item, quantity: item.quantity + 1 }
+            }
+            return item
+          })
+        } else {
+          return [...prev, { ...product, quantity: 1 }]
+        }
+      }
+
+      return [{ ...product, quantity: 1 }]
+    });
   }
 
   return (
